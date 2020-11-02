e621ef36634ab13b8aa1e6cd4ac7ab203fe955dd
camilalordelo1 <camilalordelo15@gmail.com>
2020-11-02 13:18:16 -0300

    inserting img

--- scripts/script.js
+++ scripts/script.js
@@ -4,71 +4,226 @@ var cardObject = [
         data: "01/05/2019", 
         local: "Vila Terezinha, São Paulo - SP", 
         descricao: "Fotografia tirada de uma varanda, com a câmera de um celular sem edições ou efeitos. Na face norte, com vista, para o Jardim Guarani, e a nordeste para o Centro da cidade, verticalizado. A paisagem é de Mares de Morros, com predomínio de ocupação para uso residencial no sistema de auto construção e horizontal.", 
-        image: "/img/slider/Imagem1.jpeg"
+        image: "./img/slider/Imagem1.jpeg"
     },
     {
         autor: "José Victor Santos Alves",
         data: "07/04/2019",
         local: "Av. Deputado Cantidio Sampaio, São 	Paulo - SP",
-        descricao: " Foto tirada de um ponto alto da Av. Deputado Cantidio Sampaio. A paisagem mostra parte do Jardim. Vista Alegre, num sistema de moradia popular, Circundando o cinturão verde da serra da Cantareira.",
-        image: "/img/slider/Imagem2.jpeg"
+        descricao: "A paisagem fotografada por um celular, a partir de um ponto alto da Av. Cantídio Sampaio,  mostra parte do Jardim Vista Alegre, num sistema de moradia popular, circundando o cinturão verde da serra da Cantareira.",
+        image: "./img/slider/Imagem2.jpeg"
     },
     {
         autor: "Camila Lordelo",
         data: "26/04/2019",
         local: "Freguesia do Ó, São Paulo - SP",
-        descricao: "A fotografia foi tirada em um fim de tarde chuvoso na Av. Itaberaba. A paisagem é cultural e urbana. O Uso do solo predominante é para residencia, com uma mescla de moradia horizonta e a vertical, composta por poucos prédios. Atividade econômica é composta por pequenos e médio comercio.",
-        image: "/img/slider/Imagem3.jpeg"
+        descricao: "A fotografia foi tirada em um fim de tarde chuvoso na Av. Itaberaba. A paisagem é cultural e urbana. O Uso do solo predominante é residencial, com uma mescla de moradia horizontal e a vertical, composta por poucos prédios. Atividade econômica é composta por pequeno e médio comércio.",
+        image: "./img/slider/Imagem3.jpeg"
     },
     {
-        autor: "Sophia Evangelista",
-        data: "26/10/2020",
-        local: "Jd. Brasília, Distrito Brasilândia,  São Paulo – SP",
-        descricao: "A paisagem é cultural e urbana. O Uso e ocupação do solo predominante é o residencial, com uma mescla de moradia horizontal e o vertical, é composto por poucos prédios.",
-        image: "/img/slider/Imagem4.jpeg"
+        autor: "Leandro Nogueira",
+        data: " 29/10/2020",
+        local: "Recanto dos Humildes, Distrito de Perus, São Paulo/SP",
+        descricao: "A paisagem cultural exibi uma frondosa copa de árvore, contrastando com uma área composta exclusivamente por casas, tipo sobrado sobreposto nas lajes, no sistema de autoconstrução e ocupação e sem infraestrutura, na periferia da cidade de São Paulo.",
+        image: "./img/slider/Imagem4.jpeg"
     },
     {
         autor: "Luan Santos",
         data: "29/10/2020",
         local: "Jd. Carumbé, Distrito: Brasilândia, São Paulo - SP ",
-        descricao: "A paisagem exibe elementos culturais, como moradia no sistema de autoconstrução em área de ocupação, e  no último plano a Serra da cantareira e o bioma Mata Atlântica.",
-        image: "/img/slider/Imagem5.jpeg"
+        descricao: "A paisagem exibe elementos culturais, como moradia no sistema de autoconstrução em área de ocupação, e  no último plano a Serra da Cantareira e o bioma Mata Atlântica.",
+        image: "./img/slider/Imagem5.jpeg"
     },
     {
         autor: "Matheus Cordeiro ",
         data: "18/05/2019",
         local: "Vila São João Batista, São Paulo-SP",
         descricao: "A paisagem panorâmica do distrito da Brasilândia, área de ocupação irregular e dormitório. Na direção noroeste e oeste, o colorido do por do sol emoldura o cenário do fim do dia.",
-        image: "/img/slider/Imagem6.jpeg"
+        image: "./img/slider/Imagem6.jpeg"
     },
     { 
         autor: "Carlos Kaique Duarte Pontes ", 
         data: "01/05/2019", 
-        local: "Jd. Paulistano, Distrito Brasilândia,São Paulo-SP", 
-        descricao: "A paisagem, na direção Nordeste, no primeiro plano predomina o sistema de autoconstrução em área de ocupação e horizontal.; no segundo plano destacam-se os edifícios das unidades escolares do CEU e da ETEC Paulistano e o último composto pelo florestamento de eucaliptos. ",
-        image: "/img/slider/Imagem7.jpeg"
+        local: "Jd. Paulistano, Distrito Brasilândia, São Paulo-SP", 
+        descricao: "A paisagem, na direção Nordeste, no primeiro plano predomina o sistema de autoconstrução em área de ocupação e horizontal; no segundo plano destacam-se os edifícios das unidades escolares do CEU e da ETEC, ambos Paulistano e o último é composto pelo florestamento de eucaliptos.",
+        image: "./img/slider/Imagem7.jpeg"
     },
     {
-        autor: "Grazielly Gomes ",
+        autor: "Grazielly Gomes",
         data: "28/04/2019",
-        local: " Parque de Taipas, São Paulo - SP",
-        descricao: "Está foto foi tirada de uma laje às 6:30 da manhã, direção nordeste, por um celular e sem edição. A paisagem é cultural, do Parque Taipas conhecido como Arvão. A luz difusa na escuridão matinal, exibe uma área de ocupação residencial, predominantemente horizontal.",
-        image: "/img/slider/Imagem8.jpeg"
+        local: "Parque Taipas, Distrito do Jaraguá, São Paulo/SP",
+        descricao: "Esta foto foi tirada de uma laje às 6:30 da manhã, direção nordeste, por um celular e sem edição. A paisagem é cultural, do Parque Taipas conhecido como Arvão. A luz difusa na escuridão matinal, exibe uma área de ocupação residencial, predominantemente horizontal.",
+        image: "./img/slider/Imagem8.jpeg"
     },
     {
         autor: "Pedro Henrique",
         data: "31/04/2019",
         local: "Jardim Panamericano - Jaraguá, São Paulo - SP",
         descricao: "A paisagem é  dividida em diversos planos, o primeiro é uma área composta por gramínea e terraplanagem, o segundo exibe um conjunto residencial de classe C, horizontal e vertical, o terceiro o pico do Jaraguá.",
-        image: "/img/slider/Imagem9.jpeg"
+        image: "./img/slider/Imagem9.jpeg"
     },
     {
         autor: "Yasmim Gabrielle",
         data: "27/04/2019",
-        descricao: "Foto tirada na Avenida Itaberaba, em São Paulo -SP. Predomina comércios horizontais e moradias na vertical com carros estacionados apresentando um ambiente típico urbano. Ao fundo o dia ensolarado e árvores compondo a paisagem. Foi tirada no período da tarde.",
-        image: "/img/slider/Imagem10.jpeg"
+        local: "Avenida Itaberaba, Freguesia do Ò, São Paulo - SP",
+        descricao: "A partir de uma janela, fotografou-se a Avenida Itaberaba, trecho situado no subdistrito da Freguesia do Ó. A paisagem, do final da tarde de um dia ensolarado, registra um mescla de uso residencial e do terceiro setor, pequeno e médio comércio; bem como carros estacionados, na via. ",
+        image: "./img/slider/Imagem10.jpeg"
     },
-    
+    {
+        autor: "Guilherme José",
+        data: "02/05/19 ás 14:17",
+        local: "Das Janelas da ETEC Paulistano, Jardim Paulistano, Distrito Brasilândia, São Paulo/SP",
+        descricao: "A paisagem é composta, no primeiro plano, por uma muralha de eucaliptos, espécie exótica que reduz a biodiversidade, e no segundo pelo complexo do centro unificado de educação o CEU Paulistano.",
+        image: "./img/slider/Imagem11.jpeg"
+    },
+    {
+        autor: "Rafael Miranda",
+        data: "19 de maio de 2019",
+        local: "Avenida Paulista, Jardins, São Paulo/SP",
+        descricao: "A foto foi tirada da janela de um prédio ao lado do Masp, Museu de arte de São Paulo Assis Chateaubriand, Av. Paulista, 1578, Bela Vista. A paisagem urbana, da área nobre da cidade de São Paulo, da Av. Paulista, a qual desponta, a partir de um portal de vegetação. E o alegre colorido dos grafites rompem com a monotonia das cores neutras, predominantes, nos prédios da região..",
+        image: "./img/slider/Imagem12.jpeg"
+    },
+
+    {
+        autor: "Lorena Santos Pereira",
+        data: "às 12:52 do dia 01/05/2019",
+        local: "Foto tirada a partir da Rua Dourada, Brasilândia, São Paulo/SP (zona norte)",
+        descricao: "Na paisagem observa-se as moradias escalando os mares de morro, ocupados por residencias, costuradas pelas torres de transmissão de alta tensão elétrica.",
+        image: "./img/slider/Imagem13.jpeg"
+    },
+    {
+        autor: "Gabriel Belíssimo",
+        data: "31/03/19",
+        local: "Shopping D, Sub-distrito Canindé, São Paulo/SP.",
+        descricao: "A paisagem, obtida a partir de uma das janelas do Shopping D, na direção Norte e Nordeste, exibe no primeiro plano: um trecho das Marginais e do Rio Tietê; no segundo a área é horizontal, o uso e ocupação são dos setores econômicos: secundário e terciário, e é arborizada; no terceiro destacam a verticalização, principalmente, para uso residencial, e no último os contornos da Serra da Cantareira emolduram a paisagem. Foi aplicado o filtro preto e branco, pois relembra os tempos antigos, e retoma a cor acinzentada das grandes cidades.",
+        image: "./img/slider/Imagem14.jpeg"
+    },
+    {
+        autor: "Daniel Rodrigues ",
+        data: "30/03/2018",
+        local: "Jardim Sidney, Sub-distrito Pirituba, São Paulo/SP",
+        descricao: "A paisagem é cultural, urbana e da periferia da metrópole paulistana, com predomínio do uso residencial, horizontal e no sistema de autoconstrução em área de ocupação.",
+        image: "./img/slider/Imagem15.jpeg"
+    },
+    {
+        autor: "Guilherme de Oliveira Pacheco",
+        data: "17/09/2019",
+        local: "Jd. Guarani, Distrito Brasilândia, São Paulo - SP",
+        descricao: "A partir de uma janela residencial, a paisagem captada pelas câmera de um celular, exibe a ocupação de uma área de mares de morros, por casas, e ao fundo um trecho da Serra da Cantareira, ainda preservada.",
+        image: "./img/slider/Imagem16.jpeg"
+    },
+    {
+        autor: " Vitor Hugo",
+        data: "12/03/2020",
+        local: "Distrito Brasilândia, São Paulo/SP",
+        descricao: "Na paisagem destaca-se a beleza do nascer do sol, exibindo o céu roxo-alaranjado cobrindo o distrito da Brasilândia.",
+        image: "./img/slider/Imagem17.jpeg"
+    },
+    {
+        autor: "Lucas Araújo",
+        data: "21/04/2019",
+        local: "Hipermercado Roldão, do Distrito da Brasilândia, São Paulo/SP",
+        descricao: "A paisagem é cultural da periferia da cidade de São Paulo, o uso e ocupação predominante é residencial, horizontal, parcialmente legal e com alguns prédios. E o céu claro contrasta com o verde escuro da vegetação.",
+        image: "./img/slider/Imagem18.jpeg"
+    },
+    {
+        autor: "Ana Clara Targino",
+        data: "01/05/2019",
+        local: "Distrito do Jaraguá, São Paulo/SP.",
+        descricao: "A paisagem divide-se em cultural e natural. Exibindo o uso e ocupação do solo de uma área de proteção ambiental, formando um 'bairro dormitório', ao lado da Serra da Cantareira. A paisagem cultural denuncia a insuficiência de moradia popular e os riscos do desmatamento urbano, caso da Serra da Cantareira. As nuvens em flocos e a Serra da Cantareira aproxima-se do céu, o qual é levemente tocado pela ponta das antenas de transmissão de ondas.",
+        image: "./img/slider/Imagem19.jpeg"
+    },
+    {
+        autor: "Douglas Lourenço",
+        data: "01/05/2019",
+        local: "Jardim Paulistano, Distrito Brasilândia, São Paulo/SP.",
+        descricao: "As paisagens, se diferenciam pela aplicação de um filtro, exibem casas e sobrados inacabados com suas caixas d`água, no alto das lajes,  quase captam água das nuvens.",
+        image: "./img/slider/Imagem20.jpeg"
+    },
+    {
+        autor: "Murilo Magalhães",
+        data: "01/05/19",
+        local: "Jd. Carumbé, Distrito da Brasilândia, São Paulo - SP",
+        descricao: "A paisagem engloba o Jardim Carumbé, com vista para a Vila Terezinha, ambos no Distrito da Brasilândia. Observa-se no espaço geográfico, ausência de planejamento, como paisagismo, lazer cultural e/ou físico, entre outros elementos. O uso e ocupação da área é de casa de alvenaria, de uso residencial, com edificação no sistema de autoconstrução em área de ocupação.",
+        image: "./img/slider/Imagem21.jpeg"
+    },
+    {
+        autor: "Guilherme Augusto",
+        data: "02/05/2019",
+        local: "ETEC Paulistano,  Jardim Paulistano, Distrito Brasilândia, São Paulo/SP",
+        descricao: "A partir das janelas da ETEC observa-se o quadro cuja moldura é composta por troncos e copas das árvores, enquadrando as moradias em área de ocupação e sistema de autoconstrução.",
+        image: "./img/slider/Imagem22.jpeg"
+    },
+    {
+        autor: "Felipe",
+        data: "20/03/19 por volta das 16:40h.",
+        local: "Céu Paulistano, Jardim Paulistano, Distrito da Brasilândia, São Paulo/SP.",
+        descricao: "A paisagem contém apenas elementos culturais, apesar da predominância da vegetação, pois é exótica e resulta de florestamento, seguida por uma quadra de esportes, escadaria , entre outros. Emoldurada por um céu encoberto com nuvens escuras",
+        image: "./img/slider/Imagem23.jpeg"
+    },
+    {
+        autor: "Bianca de Almeida Lopes",
+        data: "01/05/2019",
+        local: "Jd. Paulistano, Distrito Brasilândia, São Paulo - SP",
+        descricao: "Foto tirada de uma janela residencial no Jardim Paulistano, com direção para a Rua das Pedras. A paisagem retrata a organização do espaço geográfico da periferia da cidade de São Paulo. É constituída por moradias de alvenaria, no sistema autoconstrução, distribuídas nas encostas.",
+        image: "./img/slider/Imagem24.jpeg"
+    },
+    {
+        autor: "Augusto César",
+        data: "08/07/19",
+        local: "Jd. Paulistano, Distrito Brasilândia, São Paulo - SP",
+        descricao: "Paisagem mostra um longo corredor reservado ao sistema de transmissão de energia elétrica, sem ocupação residencial, por ser área de risco.  Ao longo da trilha das torres de eletricidade, predomina o sistema de ocupação e de autoconstrução.",
+        image: "./img/slider/Imagem25.jpeg"
+    },
+    {
+        autor: "Sophia Evangelista",
+        data: "26/10/2020",
+        local: "Jardim Brasília, Distrito Brasilândia, São Paulo – SP",
+        descricao: "A paisagem é cultural e urbana. O Uso e ocupação do solo predominante é o residencial, com uma mescla de moradia horizontal e vertical, é composto por poucos prédios.",
+        image: "./img/slider/Imagem26.jpeg"
+    },
+    {
+        autor: "Armando Filho",
+        data: "19/05/2020",
+        local: "Ponte Casa Verde, Casa Verde, São Paulo, SP",
+        descricao: "A paisagem é cultural da área urbana, a partir de uma das pontes do rio Tietê, exibindo um dos rios mais famosos e poluidos do Brasil. Atualmente, retilinizado, com paisagismo linear, sua água apresenta-se com tonalidade cinza chumbo e odores desagradáveis. As marginais possibilitam o transporte na área urbana e conexão com rodovias. E ao longo das marginais despontam os arranha-céu modernos e imponentes.",
+        image: "./img/slider/Imagem27.jpeg"
+    },
+    {
+        autor: "Nicolas Santos",
+        data: "29/04/2019",
+        local: "Jardim Paulistano, Distrito Brasilândia, São Paulo/SP",
+        descricao:"A partir de uma janela residencial, a câmera de um celular registrou a paisagem cultural, urbana e da periferia paulistana, com predomínio de uso residencial, e alguns comércios. Cercada pelas torres de transmissão de energia elétrica e emoldurada pelo colorido do por do sol e céu nublado.",
+        image:"./img/slider/Imagem28.jpeg"
+    },
+    {
+        autor: "Carlos Eduardo",
+        data: "16/02/2019",
+        local: "Rua Ouro Velho, Distrito Brasilândia, São Paulo/SP.",
+        descricao: "Paisagem noturna, fotografada a partir da janela, de um sobrado, localizado na Rua Ouro Velho. Uma noite com um colorido natural nos tons de azul e cinza chumbo com uma generosa faixa de nuvens brancas e azul no final do horizonte.",
+        image: "./img/slider/Imagem29.jpeg"
+    },
+    {
+        autor: "Vitor Emanuel",
+        data: "25/04/2019",
+        local: "Jardim Guarani, no Distrito da Brasilândia, São Paulo/SP.",
+        descricao: "É uma paisagem noturno com vista para a face sul, destacam-se os pontos iluminados na escuridão da cidade, um pequeno conjunto de prédios, e a Avenida João Paulo, corredor de ligação do distrito até a Marginal Tietê e o subdistrito Freguesia do Ó.",
+        image: "./img/slider/Imagem30.jpeg"
+    },
+    {
+        autor: "Rogério Leite",
+        data: "02/05/2019",
+        local: "Praça do Coqueiro na Cohab de Parada de Taipas, Distrito Jaraguá, São Paulo/SP.",
+        descricao: "Os elementos da imagem são: uma área plana e pavimentada, Praça dos Coqueiros; o muro dos prédios da Cohab exibem uma mistura de pichação; os três ou quatro pontos de iluminação desenham a silhueta das árvores ao fundo.",
+        image: "./img/slider/Imagem31.jpeg"
+    },
+    {
+        autor: "Marcos Codo",
+        data: "03/03/2020",
+        local: " Subdistrito Freguesia do Ó, São Paulo/SP.",
+        descricao: "No período da quarentena, relativa a pandemia Covid-19, o olhar através da janela foi algo possível e seguro. Observou-se na paisagem do final da tarde o envolvimento das residencias e das ruas em uma luz suave que se harmonizaram no final do horizonte em uma tênue faixa de cores como o rosa, o branco e  predomínio do azul no céu nublado.",
+        image: "./img/slider/Imagem32.jpeg"
+    },
 ];
 
 var card = document.getElementById("cards");
@@ -81,6 +236,7 @@ for (var i = 0; i <= cardObject.length; i++) {
         <div class="card-body">
         <h5 class="card-title">${cardObject[i].autor}</h5>
         <p><strong>Data</strong>: ${cardObject[i].data} </p>
+        <p><strong>Local</strong>: ${cardObject[i].local} </p>
         <p class="card-text text-justify"> ${cardObject[i].descricao}</p>
             <div class="container pl-1 text-center"> 
                 <button href="#" class="btn btn-dark" data-toggle="modal" data-target="#modal${i}"> Visualizar imagem </button>
@@ -106,11 +262,4 @@ for (var i = 0; i <= cardObject.length; i++) {
         </div>
     </div>`;
     
-}
-
-
-
-
-function curtir(posi) {
-    cardObject[posi].curtidas += 1;
 }
